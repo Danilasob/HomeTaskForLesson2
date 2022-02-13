1f0f40538a7229c4909289609648ea03e86a5327
Danilasob <sobolev27f950@mail.ru>
2022-02-13 22:23:54 +0300

    all code

---
+++ src/test/java/HomeTask2.java
@@ -0,0 +1,75 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HomeTask2 {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.baseUrl = "https://demoqa.com";  //общий адрес
+        Configuration.startMaximized = true;  //масштабизацаия браузера на максимальный размер
+
+    }
+    @Test
+    void successFillTest(){
+        open("/automation-practice-form");  //конкретный адрес
+
+        $(".main-header").shouldHave(text("Practice Form"));  //ассерт с текстовыми элементами
+        $(byText("Student Registration Form"));  //ассерт заголовка
+
+        $("#firstName").setValue("Testirovchikkk");  //Ввод фамилии
+        $("#lastName").setValue("Test");  //Ввод имени
+        $("#userEmail").setValue("Test@mail.ru");  //Ввод почты
+
+        $(byText("Other")).click();  //Выбор пола
+        $("#userNumber").setValue("89031812772");  //Ввод номера телефона
+
+        //Блок "Дата рождения"
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__year-select").selectOption("1995");
+        $$(".react-datepicker__day").find(text("27")).click();
+
+        $("#subjectsInput").setValue("English").pressEnter();  //Предмет
+
+        //Хобби
+        $(byText("Sports")).click();
+        $(byText("Reading")).click();
+        $(byText("Music")).click();
+
+        $("#uploadPicture").scrollTo();
+        $("#uploadPicture").uploadFromClasspath("img/1.jpeg");  //Загрузка файла
+
+        $("#currentAddress").setValue("Some address");  //Адрес
+
+        //Выбор штата и города
+        $("#state").click();
+        $(byText("Haryana")).click();
+        $("#city").click();
+        $(byText("Panipat")).click();
+
+        //Отправка
+        $("#submit").scrollTo();
+        $("#submit").click();
+
+        //Ассерт
+        $(".table-responsive").shouldHave(
+                text("Testirovchikkk Test"),
+                text("test@mail.ru"),
+                text("Other"),
+                text("89031812772"),
+                text("27 February,1995"),
+                text("English"),
+                text("Sports, Reading, Music"),
+                text("1.jpeg"),
+                text("Some address"),
+                text("Haryana Panipat")
+        );
+        //Закрыть модальное окно
+        $("#closeLargeModal").click();
+    }
+}
